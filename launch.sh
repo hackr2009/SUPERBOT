caebc65afe109ea651f69f00a3e6da30e04adeaf
hackr2009 <omarteam737@gmail.com>
2017-08-28 11:16:20 +0300

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,4 +1,64 @@
-#!/bin/bash
+#!/usr/bin/env bash
+token="حط توكن ههنا"
+function print_logo() {
+	echo -e "\e[38;5;77m"   echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e ""
+echo -e ""
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+}
 
-./tg -s bot.lua
+if [ ! -f ./tg ]; then
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+    echo "\e[31;1mtg not found"
+    echo "Run $0 install"
+    exit 1
+ fi
+if [ ! $token ]; then
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e 
+echo -e
+  echo -e "\e[31;1mToken Not found\e"
+ exit 1
+ fi
 
+
+  print_logo
+   echo -e ""
+echo -e ""
+echo -e 
+echo -e 
+echo -e 
+echo -e
+echo -e
+echo -e 
+echo -e 
+echo -e
+echo -e 
+echo -e    
+
+curl "https://api.telegram.org/bot"$token"/sendmessage" -F
+./tg -s ./bot.lua $@ --bot=$token
+
+
+
+ Desktop version
